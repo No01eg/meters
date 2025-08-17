e557ac931bc7b5e4a1e943a4a0f4b50172dc8405
no01eg <master.No01eg@gmail.com>
2025-08-17 14:57:45 +0300

    Добавлено  апи драйвера

---
+++ src/meters.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef enum {
+    meters_type_externAC,
+    meters_type_externDC,
+#ifdef STRIM_METERS_BUS485_ENABLE
+    meters_type_CE318,
+    meters_type_Mercury234,
+    meters_type_SPM90,
+#endif
+    meters_type_lastIndex
+}meters_type_t;
+
+typedef enum{
+    meters_currentType_DC,
+    meters_currentType_AC
+}meters_currentType_t
+
+typedef struct{
+    uint64_t energyActive;
+    float current[3];
+    float voltage[3];
+    float powerActive;
+    float frequency;
+}meters_valuesAC_t;
+    
+
+typedef struct{
+    uint64_t energy;
+    float current;
+    float voltage;
+    float power;
+}meters_valuesDC_t;
+
+typedef struct{
+    union{
+        meters_valuesAC_t AC;
+        meters_valuesDC_t DC;
+    };
+    meters_currentType_t type;
+    //uint64_t timemark;
+}meters_values_t;
+
+typedef struct{
+    meters_type_t type;
+    uint32_t address;
+    uint32_t baudrate;
+    uint32_t currentFactor;
+}meter_parameters_t;
+
+typedef struct{
+    meter_parameters_t parameters;
+    meters_values_t values;
+    int32_t isValid;
+    uint64_t timemark;
+}meter_itemInfo_t;
+
+typedef struct{
+    meter_itemInfo_t items[CONFIG_STRIM_METERS_ITEMS_MAX_COUNT];
+    uint32_t count;
+}meters_collect_values_t;
+
+typedef int32_t (*meters_get_parameters_t)(meter_parameters_t *table, 
+                                        uint32_t table_size, 
+                                        void *user_data);
+
+int32_t meters_init(meters_get_parameters_t cb, void *user_data);
+int32_t meters_reinit(void);
+int32_t meters_set_values(uint32_t idx, const meters_values_t *buffer);
+int32_t meters_get_values(uint32_t idx, meters_values_t *buffer);
+int32_t meters_get_all(meters_collect_values_t *buffer);
+const uint8_t * meters_get_typename(meters_type_t type);
