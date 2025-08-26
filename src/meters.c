03fa68216f19806c273e9b0d8eafb8b0a2545326
no01eg <master.No01eg@gmail.com>
2025-08-26 20:24:17 +0300

    Изменен порядок описания функций в модуле

--- src/meters.c
+++ src/meters.c
@@ -5,9 +5,6 @@ LOG_MODULE_REGISTER(meters, CONFIG_STRIM_METERS_LOG_LEVEL);
 
 static K_THREAD_STACK_DEFINE(Meters_BaseStack, CONFIG_STRIM_METERS_MAIN_STACK_SIZE);
 
-static void meters_baseThread(void *args0, void *args1, void *args2);
-static int32_t initializeMetersContext(meters_context_t *context);
-
 meters_context_t metersContext = {
     .baseStack = Meters_BaseStack,
     .baseStackSize = K_THREAD_STACK_SIZEOF(Meters_BaseStack),
@@ -31,37 +28,69 @@ static const meters_typeDescription_t meters_typeDescription[meters_type_lastInd
                             .read = NULL},
 };
 
-int32_t meters_init(meters_get_parameters_t cb, void *user_data){
-    meters_context_t * context = &metersContext;
+static int32_t initializeMetersContext(meters_context_t *context){
     int32_t ret;
-    
-    if(cb == NULL)
-        return -EINVAL;
+    //default properties
+    for(uint32_t i = 0; i < CONFIG_STRIM_METERS_ITEMS_MAX_COUNT; i++){
+        context->parameters[i].currentFactor = 1;
+        context->parameters[i].address = 0;
+        context->parameters[i].type = meters_type_lastIndex;
+    }
 
-    k_mutex_init(&context->dataAccessMutex);
-    k_sem_init(&context->reinitSem, 0 ,1);
+    ret = context->getParameters(context->parameters, 
+                                CONFIG_STRIM_METERS_ITEMS_MAX_COUNT,
+                                context->user_data);
+    if(ret < 0){
+        LOG_ERR("get parameters error: %d\r\n", ret);
+        return ret;
+    }
 
-    context->getParameters = cb;
-    context->user_data = user_data;
+    context->itemCount = ret;
 
-    #ifdef CONFIG_STRIM_METERS_BUS485_ENABLE
-    //TODO! init meters with bus485
-    #endif
-
-    (void)ret;
-
-    k_thread_create(&context->baseThread, context->baseStack, context->baseStackSize,
-                    meters_baseThread, context, NULL, NULL,
-                    CONFIG_STRIM_METERS_INIT_PRIORITY, 0, K_NO_WAIT);
-    
-    k_thread_name_set(&context->baseThread, "meters");
+    for(uint32_t i = 0; i < context->itemCount; i++){
+        meters_type_t type = context->parameters[i].type;
+        if(type >= meters_type_lastIndex){
+            LOG_ERR("meter %u have unknown type %u\r\n", i, context->parameters[i].type);
+            context->itemCount = 0;
+            return -1; //TODO set error type
+        }
+    }
     return 0;
 }
 
-int32_t meters_reinit(void){
-    meters_context_t *context = &metersContext;
-    k_sem_give(&context->reinitSem);
-    return 0;
+static void meters_baseThread(void *args0, void *args1, void *args2){
+    meters_context_t *context = (meters_context_t*)args0;
+    (void)args1;
+    (void)args2;
+
+    int32_t ret = 0;
+    bool isInitialize = false;
+
+    while(true){
+        ret = k_sem_take(&context->reinitSem, K_MSEC(250));
+        if (ret == 0)
+            isInitialize = false;
+ 
+        if(!isInitialize){
+            ret = initializeMetersContext(context);
+            if(ret < 0)
+                goto exitBaseThread;
+            isInitialize = true;
+        }
+
+        //meters data call
+        #ifdef CONFIG_STRIM_METERS_BUS485_ENABLE
+        for(uint32_t i = 0; i < context->itemsCount; i++){
+            //TODO! set periodic call meters data
+
+            k_sleep_(K_MSEC(1000));
+        }
+        #endif
+
+    }
+
+    exitBaseThread:
+    LOG_ERR("thread %s stopped\r\n", log_strdup(k_thread_name_get(k_current_get())));
 }
 
 int32_t meters_set_values(uint32_t idx, const meters_values_t *buffer){
@@ -141,68 +170,35 @@ int32_t meters_get_all(meters_values_collection_t *buffer){
     return 0;
 }
 
-
-static void meters_baseThread(void *args0, void *args1, void *args2){
-    meters_context_t *context = (meters_context_t*)args0;
-    (void)args1;
-    (void)args2;
-
-    int32_t ret = 0;
-    bool isInitialize = false;
-
-    while(true){
-        ret = k_sem_take(&context->reinitSem, K_MSEC(250));
-        if (ret == 0)
-            isInitialize = false;
- 
-        if(!isInitialize){
-            ret = initializeMetersContext(context);
-            if(ret < 0)
-                goto exitBaseThread;
-            isInitialize = true;
-        }
-
-        //meters data call
-        #ifdef CONFIG_STRIM_METERS_BUS485_ENABLE
-        for(uint32_t i = 0; i < context->itemsCount; i++){
-            //TODO! set periodic call meters data
-
-            k_sleep_(K_MSEC(1000));
-        }
-        #endif
-
-    }
-
-    exitBaseThread:
-    LOG_ERR("thread %s stopped\r\n", log_strdup(k_thread_name_get(k_current_get())));
+int32_t meters_reinit(void){
+    meters_context_t *context = &metersContext;
+    k_sem_give(&context->reinitSem);
+    return 0;
 }
 
-static int32_t initializeMetersContext(meters_context_t *context){
+int32_t meters_init(meters_get_parameters_t cb, void *user_data){
+    meters_context_t * context = &metersContext;
     int32_t ret;
-    //default properties
-    for(uint32_t i = 0; i < CONFIG_STRIM_METERS_ITEMS_MAX_COUNT; i++){
-        context->parameters[i].currentFactor = 1;
-        context->parameters[i].address = 0;
-        context->parameters[i].type = meters_type_lastIndex;
-    }
+    
+    if(cb == NULL)
+        return -EINVAL;
 
-    ret = context->getParameters(context->parameters, 
-                                CONFIG_STRIM_METERS_ITEMS_MAX_COUNT,
-                                context->user_data);
-    if(ret < 0){
-        LOG_ERR("get parameters error: %d\r\n", ret);
-        return ret;
-    }
+    k_mutex_init(&context->dataAccessMutex);
+    k_sem_init(&context->reinitSem, 0 ,1);
 
-    context->itemCount = ret;
+    context->getParameters = cb;
+    context->user_data = user_data;
 
-    for(uint32_t i = 0; i < context->itemCount; i++){
-        meters_type_t type = context->parameters[i].type;
-        if(type >= meters_type_lastIndex){
-            LOG_ERR("meter %u have unknown type %u\r\n", i, context->parameters[i].type);
-            context->itemCount = 0;
-            return -1; //TODO set error type
-        }
-    }
+    #ifdef CONFIG_STRIM_METERS_BUS485_ENABLE
+    //TODO! init meters with bus485
+    #endif
+
+    (void)ret;
+
+    k_thread_create(&context->baseThread, context->baseStack, context->baseStackSize,
+                    meters_baseThread, context, NULL, NULL,
+                    CONFIG_STRIM_METERS_INIT_PRIORITY, 0, K_NO_WAIT);
+    
+    k_thread_name_set(&context->baseThread, "meters");
     return 0;
 }
