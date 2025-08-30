bb0f33382129f0d8e5ec2ece414925bc84080bc5
no01eg <master.No01eg@gmail.com>
2025-08-30 10:01:20 +0300

    добавлена проверка типа значений на соответствие типа использующего счетчик

--- src/meters.c
+++ src/meters.c
@@ -102,8 +102,8 @@ int32_t meters_set_values(uint32_t idx, const meters_values_t *buffer){
     if(idx >= context->itemCount)
         return -ERANGE;
     
-    if(buffer->type != meters_typeDescription[idx].valuesType)
-        return -EINVAL;
+    if(buffer->type != meters_typeDescription[context->parameters[idx].type].valuesType)
+        return -EINVAL; 
     
     k_mutex_lock(&context->dataAccessMutex, K_FOREVER);
     {
