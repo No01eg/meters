095e7622134c2dc3df2d15ec0f4eacf1fcba39f1
no01eg <master.No01eg@gmail.com>
2025-09-07 21:03:53 +0300

    добавлена функция конвертации из dff значений

--- src/meter485/meters_ce318.c
+++ src/meter485/meters_ce318.c
@@ -3,6 +3,8 @@
 LOG_MODULE_DECLARE(meters, CONFIG_STRIM_METERS_LOG_LEVEL);
 
 enum{CE318_ERROR_THRESHOLD = 3};
+#define DFF_FLAG (0x80)
+#define DFF_FIELD_MAX_SIZE (sizeof(int64_t) + 1)
 
 static int32_t ce318_set_escape(const uint8_t * src, uint8_t * dest, int32_t count)
 {
@@ -66,6 +68,44 @@ static uint16_t ce318_get_crc16(const uint8_t * buffer, uint32_t lenght)
     return crc;
 }
 
+static int32_t ce318_dff_parce(const uint8_t * buffer, uint32_t remaining,
+                                int64_t *field, int32_t signed_field)
+{
+    if(buffer == NULL)
+        return -1;
+    
+    if(field == NULL)
+        return -2;
+    
+    const uint8_t *ch = buffer;
+    int32_t field_size = 0;
+    uint64_t result = 0;
+    uint8_t minus = false;
+
+    if(signed_field){
+        minus = (*ch & 0x40) != 0;
+    }
+
+    if(remaining == 0)
+        return -4;
+
+    do{
+        if(field_size > DFF_FIELD_MAX_SIZE)
+            return -3;
+        
+        result |= (*ch & 0x7F) << (field_size * 7);
+        field_size++;
+        
+    }while((*ch++ & DFF_FLAG) != 0);
+
+    if(minus){
+       result |= 0xffffffffffffffff << (field_size * 7);
+    }
+
+    *field = (int64_t)result;
+
+    return field_size;
+}
 
 int32_t meters_ce318_init(meters_context_t * context, uint32_t item_idx)
 {
